e2686b090fdcef5b0be0d09d8ad6b48d19f37ed6
HughZhang <40616309+HughZhang98@users.noreply.github.com>
2024-02-29 11:25:27 +0800

    change website icon and title

--- Vue3_TS_ElementUI+/src/router/index.ts
+++ Vue3_TS_ElementUI+/src/router/index.ts
@@ -9,27 +9,32 @@ const routes: Array<RouteRecordRaw> = [
     {
       path: '/articles/index',
       name: 'list-article',
-      component: () => import('../views/ListArticle.vue') 
+      component: () => import('../views/ListArticle.vue'),
+      meta: { title: "Article List" } 
     },
     {
       path: '/articles/create',
       name: 'create-article',
-      component: () => import('../views/CreateArticle.vue') 
+      component: () => import('../views/CreateArticle.vue'),
+      meta: { title: "New Article" }  
     },
     {
       path: '/articles/:id/edit',
       name: 'edit-article',
-      component: () => import('../views/EditArticle.vue') 
+      component: () => import('../views/EditArticle.vue'),
+      meta: { title: "Article Edit" }  
     },
     {
       path: '/login',
       name: 'login',
-      component: () => import('../views/Login.vue')
+      component: () => import('../views/Login.vue'),
+      meta: { title: "Login" } 
     },
     {
       path: '/signUp',
       name: 'signUp',
-      component: () => import('../views/SignUp.vue')
+      component: () => import('../views/SignUp.vue'),
+      meta: { title: "Sign Up" } 
     }
 ]
 
@@ -37,5 +42,7 @@ const router = createRouter({
     history: createWebHashHistory(), // 路由模式 hash模式
     routes: routes
 })
-
+router.afterEach((to, from) => {
+  document.title = to.meta.title as string || 'Vue 3 + TypeScript + Vite'
+})
 export default router;
